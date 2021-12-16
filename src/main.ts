25dbd67e3cc4ae966f1c1733dc191efb8c2cb918
Tim Morgan <git@timothymorgan.info>
2021-12-16 15:13:35 -0800

    Travis CI -> GitHub Actions

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from './router'
 import '@/config/bugsnag'
 import 'normalize.css'
 import '@/assets/styles/fonts.scss'
-import '@/assets/styles/common.sass'
+import '@/assets/styles/common.scss'
 import i18n from '@/i18n'
 
 Vue.config.productionTip = false
